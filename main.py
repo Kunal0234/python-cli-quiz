22f1907819427ccdeec4e0dea2c4ff2c6e319406
Kunal Kumar <kunalsinghlords@gmail.com>
2026-09-21 19:58:21 +0530

    Add first quiz question

---
+++ main.py
@@ -0,0 +1,17 @@
+
+print("==============================\n    PYTHON CLI QUIZ  \n============================== \n")
+print("Welcome to the python Quiz!\n")
+print("You will answer multiple-choice questions.")
+print("Let's begin!\n")
+print(""" 
+Question 1:
+
+What is Python?
+
+1. Programming Language
+2. Database
+3. Operating System
+4. Web Browser 
+
+""")
+answer1 = input("Enter your answer :- ")
